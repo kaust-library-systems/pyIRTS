60c2ba2852c072625f4eaf70ed4ba9292243f1b4
Marcelo Garcia <marcelo.garcia@kaust.edu.sa>
2025-10-23 17:24:06 +0300

    update

--- harvest.py
+++ harvest.py
@@ -3,8 +3,21 @@
 #
 
 import typer
+import requests
 
+class arxiv:
+    def __init__(self, URL: str, queryItem: str):
+        self.URL = URL,
+        self.queryItem = queryItem
+
+    def get_metadata(self):
+        arxivBaseURL = "http://export.arxiv.org/api/search_query="
+        metadata = requests.get(self.URL)
+
+        
+        
 def main(source: str):
+    
     print(f"Source: {source}")
 
 if __name__ == "__main__":
